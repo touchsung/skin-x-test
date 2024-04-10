b347a212ba869a8142ce564358c81c9ec6dc2c9c
jettapat <jettapat.th@gmail.com>
2024-04-10 23:42:14 +0700

    feat: connect and seed db with prisma

---
+++ backend/prisma/migrations/20240410163746_init/migration.sql
@@ -0,0 +1,43 @@
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "postedAt" TIMESTAMP(3) NOT NULL,
+    "postedById" INTEGER NOT NULL,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Author" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Author_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Tag" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Tag_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "TagsOnPosts" (
+    "postId" INTEGER NOT NULL,
+    "tagId" INTEGER NOT NULL,
+
+    CONSTRAINT "TagsOnPosts_pkey" PRIMARY KEY ("postId","tagId")
+);
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_postedById_fkey" FOREIGN KEY ("postedById") REFERENCES "Author"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "TagsOnPosts" ADD CONSTRAINT "TagsOnPosts_postId_fkey" FOREIGN KEY ("postId") REFERENCES "Post"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "TagsOnPosts" ADD CONSTRAINT "TagsOnPosts_tagId_fkey" FOREIGN KEY ("tagId") REFERENCES "Tag"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ backend/prisma/seed.ts
@@ -0,0 +1,66 @@
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+import * as posts from './posts.json';
+
+interface PostData {
+  title: string;
+  content: string;
+  postedAt: string;
+  postedBy: string;
+  tags: string[];
+}
+
+async function seed() {
+  try {
+    for (const postData of posts as PostData[]) {
+      const {
+        title,
+        content,
+        postedAt,
+        postedBy: authorName,
+        tags: tagNames,
+      } = postData;
+
+      // Find or create the author
+      let author = await prisma.author.findFirst({
+        where: { name: authorName },
+      });
+
+      if (!author) {
+        author = await prisma.author.create({ data: { name: authorName } });
+      }
+
+      // Create the post
+      await prisma.post.create({
+        data: {
+          title,
+          content,
+          postedAt: new Date(postedAt),
+          postedBy: { connect: { id: author.id } },
+          TagsOnPosts: {
+            create: await Promise.all(
+              tagNames.map(async (tagName) => {
+                let tag = await prisma.tag.findFirst({
+                  where: { name: { equals: tagName } },
+                });
+                if (!tag) {
+                  tag = await prisma.tag.create({ data: { name: tagName } });
+                }
+                return { tag: { connect: { id: tag.id } } };
+              }),
+            ),
+          },
+        },
+      });
+
+      console.log(`Post "${title}" created successfully.`);
+    }
+    console.log('Seed data inserted successfully.');
+  } catch (error) {
+    console.error('Error seeding data:', error);
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+seed();
